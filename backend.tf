44ed36996c03ab3b9a6163947f642da552d7caca
Shiv4y <85942630+Shiv4y@users.noreply.github.com>
2021-06-18 21:07:38 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#    backend "azurerm" {
-#      resource_group_name  = "tf_statestore018"
-#      storage_account_name = "tfstate018"
-#      container_name       = "tfstate018"
-#      key                  = "terraform.tfstate"
-#    }
-# }
